0761f7de4c14fae3859205e93ab9fe64fb67bf12
email <tarunmehto71@gmail.com>
2023-10-06 10:28:05 +0530

    error handaling

--- route/employee.route.js
+++ route/employee.route.js
@@ -8,7 +8,7 @@ employeeRouter.get("/employee", async (req, res) => {
   try {
     const allEmployees = await EmployeeModel.find();
     if (!allEmployees || allEmployees.length === 0) return res.status(400).send("No employees type was found!")
-    res.status(200).json(allEmployees);
+    res.status(200).send(allEmployees);
   } catch (error) { 
     res.status(500).send(`Internal Server Error${error.message}` );
   }
@@ -20,7 +20,7 @@ employeeRouter.post("/employee", async (req, res) => {
   try {
     const employee = new EmployeeModel(payload);
     await employee.save();
-    res.status(200).json({ msg: "Employee Posted Successfully" });
+    res.status(200).send("Employee Posted Successfully");
   } catch (error) {
    
     res.status(500).send(`Internal Server Error${error.message}` );
@@ -35,7 +35,7 @@ employeeRouter.patch("/employee/", async (req, res) => {
   try {
     const updatedEmployee = await EmployeeModel.findByIdAndUpdate(id, payload);
     if (!updatedEmployee) {
-      return res.status(400).json({ error: "Employee not found" });
+      return res.status(400).send(`Employee not found ${error.message}`);
     }
     res.status(200).json({ msg: "Employee Update successfully" });
   } catch (error) {
@@ -51,7 +51,7 @@ employeeRouter.delete("/employee", async (req, res) => {
   try {
     const deletedEmployee = await EmployeeModel.findByIdAndRemove(id);
     if (!deletedEmployee) {
-      return res.status(400).json({ error: "Employee not found" });
+      return res.status(400).send(`Employee not found ${error.message}`);
     }
     res.status(200).json({ msg: "Employee Delete successfully" });
   } catch (error) {
--- route/user.route.js
+++ route/user.route.js
@@ -9,7 +9,7 @@ userRouter.get("/user", async (req, res) => {
     let all_user = await User.find();
     if (!all_user || all_user.length === 0) return res.status(400).send("No employees type was found!")
 
-    res.status(200).json(all_user);
+    res.status(200).send(all_user);
   } catch (error) {
     res.status(500).send(`Internal Server Error${error.message}` );
   }
@@ -22,7 +22,7 @@ userRouter.post("/user", async (req, res) => {
   try {
     let user = new User(payload);
     await user.save();
-    res.status(200).json({ msg: "Employee Posted Succssfully" });
+    res.status(200).send("Employee Posted Succssfully");
   } catch (error) {
     res.status(500).send(`Internal Server Error${error.message}` );
   }
@@ -36,9 +36,9 @@ userRouter.patch("/user", async (req, res) => {
   try {
     let updateUser = await User.findByIdAndUpdate({ "_id": id }, payload);
     if (!updateUser) {
-      return res.status(400).json({ error: "User not found" });
+      return res.status(400).send(`Employee not found ${error.message}`);
     }
-    res.status(200).json({ msg: "Employee Update successfully" });
+    res.status(200).send("Employee Update successfully");
   } catch (error) {
     res.status(500).send(`Internal Server Error${error.message}` );
   }
@@ -51,9 +51,9 @@ userRouter.delete("/user", async (req, res) => {
   try {
     let deleteUser = await User.findByIdAndUpdate({ "_id": id });
     if (!deleteUser) {
-      return res.status(400).json({ error: "User not found" });
+      return res.status(400).send(`Employee not found ${error.message}`);
     }
-    res.status(200).json({ msg: "Employee Delete successfully" });
+    res.status(200).send("Employee Delete successfully");
   } catch (error) {
     res.status(500).send(`Internal Server Error${error.message}` );
   }
