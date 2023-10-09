b49a0af8913217802d5a6975de617b2283ff9736
email <tarunmehto71@gmail.com>
2023-10-09 12:06:28 +0530

    done

--- route/user.route.js
+++ route/user.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { User } = require("../model/user.model");
-
+const bcrypt = require("bcrypt")
 const userRouter = express.Router();
 
 //get all users
@@ -56,6 +56,46 @@ userRouter.delete("/user", async (req, res) => {
     res.status(200).send("Employee Delete successfully");
   } catch (error) {
     res.status(500).send(`Internal Server Error${error.message}` );
+  }
+});
+
+//reset password api
+
+app.post("/reset-password", async (req, res) => {
+  try {
+    const { currentPassword, newPassword, confirmPassword, sessionId } = req.body;
+
+    // Check  newPassword and confirmPassword 
+    if (newPassword !== confirmPassword) {
+      return res.status(400).send(`New password and confirm password do not match ${error.message}`);
+    }
+
+    // get user from the database 
+    const user = await User.findOne({sessionId}); 
+
+    if (!user) {
+      return res.status(401).send( `User not found.${error.message}` );
+    }
+
+    // Check current password is correct
+    const isPasswordValid = await bcrypt.compare(currentPassword, user.passwordHash);
+
+    if (!isPasswordValid) {
+      return res.status(400).send(`Current password is incorrect.${error.message}` );
+    }
+
+    // Hash the new password
+    const saltRounds = 10; 
+    const newPasswordHash = await bcrypt.hash(newPassword, saltRounds);
+
+    // Update user password in the database
+    user.passwordHash = newPasswordHash;
+    await user.save();
+
+    res.status(200).send(`Password reset successfully.` );
+  } catch (error) {
+    
+    res.status(500).send(`Internal Server Error${error.message}`);
   }
 });
 
